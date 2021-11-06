66591d486c2099daed57672e537471535f109b7a
lovina <lovinaafrifa223344@gmail.com>
2021-11-06 04:39:48 -0700

    first commit

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,13 @@ import {createStore, applyMiddleware, compose} from "redux";
 import UsersReducer from "../reducers/UsersReducer";
 import firebase from "../firebase/Config";
 import { getFirebase, reduxReactFirebase } from "react-redux-firebase";
-import{getFirestore, reduxFirestore} from "react-redux-firebase";
+import{getFirestore, reduxFirestore} from "redux-firestore";
 import thunk from "redux-thunk";
 
 var store = createStore(
 UsersReducer, 
     compose(
-applyMiddleware(thunk.withExtraArgument(getFirestore, getfirebase)),
+applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
 reduxReactFirebase(firebase),
 reduxFirestore(firebase)
     )
