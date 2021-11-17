31c64311aa2c7e3b73f590a135b277b9e74b96e1
lovina <lovinaafrifa223344@gmail.com>
2021-11-17 00:13:45 -0800

    action commit

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,48 +1,58 @@
-import { getFirebase } from "react-redux-firebase";
+export const addUser = (newUser) => {
+    //returns the type of action and data to send to the store
+    return (dispatch, state, { getFirestore }) => {
+      getFirestore()
+      .collection("users")
+      .add({ ...newUser, timestamp: getFirestore().FieldValue.serverTimestamp() })
+      .then(
+        () => {},
+        () => {}
+      )
+      // type: "ADD_USER",
+      // payload: newUser,
+    };
+  };
 
-export const addUser =(user) => {
-    // console.log("addUser", user);
-    return(dispatch,state, {getFirestore}) => {
-        getFirestore()
+  export const deleteUser = (userId) => {
+    return (dispatch, state, { getFirestore }) => {
+      getFirestore()
         .collection("users")
-        .add(user)
-        .then(
-            () => {},
-            () => {}
+        .doc(userId)
+        .delete()
+        .then(() => {});
+    
+      // type: "DELETE_USER",
+      // payload: userId,
+    };
+  };
+  
+  export const editUser = (userId, updatedUser) => {
+    return (dispatch, state, { getFirestore }) => {
+      getFirestore()
+        .collection("users")
+        .doc(userId)
+        .update(updatedUser)
+        .then(() => {});
+      // type: "EDIT_USER",
+      // payload: {userId, updatedUser},
+    };
+  };
+
+  export const getAllUsers = () => {
+    return (dispatch, state, { getFirestore }) => {
+      getFirestore()
+        .collection("users")
+        .orderBy("timestamp", "desc")
+        .onSnapshot(
+          (snapshot) => {
+            let users = [];
+            snapshot.forEach((doc) => {
+              users.push({...doc.data(), id: doc.id});
+            });
+            console.log(users);
+            dispatch({ type: "SET_ALL_USERS", payload: users });
+          },
+          (error) => {}
         );
     };
-    //  type: "ADD_USER",
-    // payload: user,
-
-}
-
-export const deleteUser =(userId) =>{
-    return{
-        type :"DELETE_USER",
-        payload: userId,
-    }
-};
-
-export const editUser =(userId,updatedUser) =>{
-    return{
-        type :"EDIT_USER",
-        payload: {userId,updatedUser}
-    }
-};
-
-export const getAllUsers =() => {
-    return(dispatch,state, {getFirestore}) => {
- getFirestore().collection("users").onSnapshot((snapshot)=>{
-     let users =[];
-     snapshot.forEach((doc)=>{  
-         users.push(doc.data())
-     })
-     dispatch({
-         type: "GET_ALL_USERS",
-         payload: users,
-     });
-
- })
-    }
-
-}
+  };
